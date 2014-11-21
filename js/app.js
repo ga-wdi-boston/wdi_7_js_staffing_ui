9dca83712f61270b0333b28f078aa5afec7a7bf0
Dan Johnson <dan@balancerockmedia.com>
2014-11-21 12:07:55 -0500

    edits

--- js/app.js
+++ js/app.js
@@ -12,9 +12,7 @@ angular.module('StaffingUI', [
 ) {
 	$rootScope.$on('$routeChangeStart', function(event, next) {
 		if (AuthFactory.isAuthenticated()) {
-			if (AuthFactory.isAuthenticated()) {
-				$http.defaults.headers.common['Authorization'] = 'Token token=' + $window.sessionStorage.getItem('staffingUI.user');
-			}
+			$http.defaults.headers.common.Authorization = 'Token token=' + $window.sessionStorage.getItem('staffingUI.user');
 
 			UserFactory.fetch();
     		TitleFactory.fetch();
